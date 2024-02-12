74f16286fea1f92d784ed37f4914b0c45e636de1
Stupak_Vitalii <iamvitalos@gmail.com>
2024-02-12 20:15:17 +0100

    car_garage_expense

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 class BasePage(object):
@@ -7,9 +9,14 @@ class BasePage(object):
         self.base_url = base_url
         self.driver = driver
         self.timeout = 30
+        self.wait = WebDriverWait(self.driver, 6)
 
-    def find_element(self, *locator):
-        return self.driver.find_element(*locator)
+    def find_element(self, locator):
+        self.wait_for_page_loading()
+        return self.wait.until(expected_conditions.presence_of_element_located(locator))
+
+    def wait_for_page_loading(self):
+        self.wait.until(lambda x: x.execute_script('return document.readyState;') == 'complete')
 
     def open(self, url):
         url = self.base_url + url
--- utils/locators.py
+++ utils/locators.py
@@ -3,12 +3,55 @@ from selenium.webdriver.common.by import By
 
 class HomePageLocators(object):
     LOGO = (By.XPATH, "//a[@class='header_logo']")
-    SIGNUP = (By.XPATH, "//button[text()='Sign Up']")
+    SIGNUP = (By.XPATH, "//button[text()='Sign up']")
     SIGNIN = (By.XPATH, "//button[contains(@class, 'header_signin')]")
 
 
 class LoginPageLocators(object):
     EMAIL = (By.ID, "signinEmail")
     PASSWORD = (By.ID, "signinPassword")
-    LOGIN = (By.CSS_SELECTOR, "buttom.btn.btn-primary")
+    LOGIN = (By.CSS_SELECTOR, "button.btn.btn-primary")
     ERROR_MESSAGE = (By.XPATH, "//p[contains(@class, 'alert-danger')]")
+
+
+class SignupPageLocators(object):
+    NAME = (By.ID, "signupName")
+    LAST_NAME = (By.ID, "signupLastName")
+    EMAIL = (By.ID, "signupEmail")
+    PASSWORD = (By.ID, "signupPassword")
+    REPEAT_PASSWORD = (By.ID, "signupRepeatPassword")
+    REGISTER = (By.XPATH, "//button[text()='Register']")
+    ERROR_MESSAGE = (By.XPATH, "//p[contains(@class, 'alert-danger')]")
+
+class ExpensesPageLocators(object):
+    HEADER = (By.XPATH, '//h1')
+    CAR_SELECTION = (By.ID, "carSelectDropdown")
+    ADD_NEW_EXPENSE = (By.XPATH, "//button[text()='Add an expense']")
+    VEHICLE = (By.ID, "addExpenseCar")
+    REPORT_DATE = (By.ID, "addExpenseDate")
+    MILEAGE = (By.ID, "addExpenseMileage")
+    NUMBER_OF_LITTERS = (By.ID, "addExpenseLiters")
+    TOTAL_COST = (By.ID, "addExpenseTotalCost")
+    ADD_BUTTON = (By.XPATH, "//button[text()='Add']")
+    EXPENSE_TABLE = (By.XPATH, "//table[@class='table expenses_table']")
+    FIRST_ROW = (By.XPATH, '(//tbody/tr)[1]')
+    DATE_COLUMN = (By.XPATH, '(//td)[1]')
+    MILEAGE_COLUMN = (By.XPATH, '(//td)[2]')
+    LITERS_COLUMN = (By.XPATH, '(//td)[3]')
+    TOTAL_COST_COLUMN = (By.XPATH, '(//td)[4]')
+
+class GaragePageLocators(object):
+    HEADER = (By.XPATH, '//h1')
+    ADD_CAR_BUTTON = (By.CSS_SELECTOR, ".btn.btn-primary")
+    BRAND_SELECT = (By.ID, "addCarBrand")
+    MODEL_SELECT = (By.ID, "addCarModel")
+    MILEAGE_TEXTBOX = (By.ID, "addCarMileage")
+    ADD_BUTTON = (By.XPATH, "//button[text()='Add']")
+
+    CAR_ADDED = (By.XPATH, "(//div[@class='car jumbotron'])[1]")
+    BRAND_ADDED = (By.XPATH, "//img[@class='car-logo_img']")
+    MODEL_ADDED = (By.XPATH, "//p[@class='car_name h2']")
+    MILEAGE_ADDED = (By.XPATH, "//input[contains(@class,'update-mileage-form_input')]")
+
+    GARAGE_MENU = (By.XPATH, "//a[@href='/panel/garage' and contains(@class, 'btn-sidebar')]")
+    FUEL_EXPENSE_MENU = (By.XPATH, "//a[@href='/panel/expenses' and contains(@class, 'btn-sidebar')]")
